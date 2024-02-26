38f673aed4c9f8ed6c71ce06566e70748dc7467a
iBadr49 <badr.amara@hva.nl>
2024-02-26 15:28:26 +0100

    Services
    Ik de services [section] -gemaakt.

---
+++ src/lib/components/services.svelte
@@ -0,0 +1,83 @@
+<section>
+    <h2>Our Services</h2>
+    <div class="services">
+        <div class="card">
+            <h3>Social Media Marketing</h3>
+            <p>
+                Nowadays, social media platforms are the most useful ways to spread your brand. It is also
+                the perfect way to create engagement with your potential customers. Our social media experts
+                will make sure that your brand’s portrayal will reach its designated audience. BrandFlowr
+                does everything from creating content to a full management of your social media accounts.
+                With doing this we help you with creating a better presence on platforms like Instagram,
+                Facebook and more.
+            </p>
+        </div>
+        <div class="card">
+            <h3>Web Development</h3>
+            <p>
+                Having a website for your business is a mandatory thing in the 21st century. First thing
+                potential customers do when they hear about a company is visit their website. This way your
+                website is not only your first impression, but your online business card as well. BrandFlowr
+                creates well-developed websites, which will bring your business’ online presence to life.
+            </p>
+        </div>
+        <div class="card">
+            <h3>Canon</h3>
+            <p>
+                Website development: Having a website for your business is a mandatory thing in the 21st
+                century. First thing potential customers do when they hear about a company is visit their
+                website. This way your website is not only your first impression, but your online business
+                card as well. BrandFlowr creates well-developed websites, which will bring your business’
+                online presence to life.
+            </p>
+        </div>
+    </div>
+</section>
+ 
+<style>
+    section {
+        background-color: #4ecca3;
+        padding: 20px;
+    }
+    .services {
+        display: flex;
+        line-height: 1.5;
+    }
+    .card {
+        border: 2px solid #38424f;
+        width: 600px;
+        justify-content: center;
+        margin: 20px;
+        border-radius: 2rem;
+        padding: 1rem;
+    }
+ 
+    h2 {
+        text-align: center;
+ 
+        font-family: 'Montserrat', sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 700;
+        font-style: normal;
+        color: #38424f;
+        font-size: 5rem;
+    }
+ 
+    h3 {
+        text-align: center;
+ 
+        font-family: 'Montserrat', sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 700;
+        font-size: 2rem;
+        color: #38424f;
+        font-style: normal;
+    }
+ 
+    p {
+        font-family: 'Montserrat', sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 300;
+        color: #38424f;
+        font-style: normal;
+    }
