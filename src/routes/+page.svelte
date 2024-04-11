e5e8081c82b4ebbda70923b29be7fa6cfc06a715
SanHont <dehontsander@gmail.com>
2024-04-11 15:03:18 +0200

    Version Midterm
    Dit was de website tijdens de midterm assesment.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import Aboutus from '../lib/components/aboutus.svelte';
 	import Services from '../lib/components/services.svelte';
 	import Footer from '../lib/components/footer.svelte';
+
 </script>
 
 <Title />
