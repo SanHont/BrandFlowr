a54d95e2e0beddaeb2a29393eb2e3d1c0dde2c16
SanHont <dehontsander@gmail.com>
2024-10-15 16:49:48 +0200

    Update footer.svelte

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,156 +1,149 @@
-<img class="wavefooter" src="assets/wavefooter.svg" alt="" loading="lazy" />
 <section>
-	<div class="logo">
-		<svg
-			width="321.75"
-			height="37.19982485900054"
-			viewBox="0 0 370 42.77835337320963"
-			loading="lazy"
-			class="iconsvg"
-			><defs id="SvgjsDefs1021"></defs><g
-				id="SvgjsG1022"
-				featurekey="QMusi1-0"
-				transform="matrix(-0.7777777777777778,0,0,0.7777777777777778,73.8888888888889,-17.533443874782986)"
-				fill="#38424f"
-				><path
-					xmlns="http://www.w3.org/2000/svg"
-					d="M46.39,49.658c2.556,0,4.727-1.633,5.506-3.907h0.028c0.016-0.077,0.028-0.155,0.047-0.229  c0.053-0.288,0.116-0.57,0.185-0.846c1.367-5.679,4.89-9.722,9.023-9.722c3.439,0,6.449,2.812,8.154,7.028  c-7.284-9.487-14.172-2.536-13.643-2.64c0.53-0.104,6.561,1.446,6.769,9.914s-5.527,10.947-7.821,11.812  c7.508,0.35,17.519-4.687,17.311-15.433c-0.132-6.796-3.753-11.038-6.4-13.243c-0.208-0.177-0.422-0.346-0.64-0.512  c-0.046-0.035-0.087-0.064-0.132-0.098c-0.294-0.22-0.593-0.433-0.903-0.631c-0.458-0.298-0.812-0.5-0.972-0.585  c-0.003-0.002-0.007-0.003-0.01-0.005c-0.057-0.03-0.093-0.048-0.093-0.048s-0.002,0.002-0.002,0.003  c-2.119-1.12-4.536-1.759-7.105-1.759c-8.353,0-15.123,6.704-15.123,14.973l0.003,0.003c-0.003,0.053-0.003,0.105-0.003,0.158  C40.568,47.077,43.174,49.658,46.39,49.658z"
-				></path><path
-					xmlns="http://www.w3.org/2000/svg"
-					d="M67.782,22.543c-5.068,0-9.814,1.376-13.875,3.764c0.97-0.133,1.958-0.208,2.966-0.208c11.707,0,21.195,9.397,21.195,20.99  c0,4.319-1.317,8.331-3.576,11.667c-0.084,0.118-0.166,0.238-0.25,0.362c-0.028,0.04-0.053,0.077-0.084,0.118  c-1.017,1.478-2.137,2.686-3.348,3.665c-6.482,5.242-15.502,3.879-25.097,2.026c-0.094-0.018-0.191-0.034-0.284-0.056  c-6.138-1.19-12.501-2.553-18.583-2.5C9.327,62.527,5,75.694,5,75.694s15.12-10.224,34.517-2.788  c15.952,6.116,27.635,5.164,34.176,2.909c0.109-0.037,0.216-0.046,0.313-0.071c0.2-0.047,0.372-0.087,0.522-0.124  c0.291-0.074,0.482-0.127,0.573-0.155c0.019-0.006,0.031-0.009,0.044-0.012c0.006,0,0.013-0.003,0.013-0.003l0.003-0.003  C86.606,72.264,95,61.852,95,49.497C95,34.613,82.815,22.543,67.782,22.543z"
-				></path></g
-			>
-		</svg>
-		<h3 class="logo-title">BRANDFLOWR</h3>
-	</div>
-	<div class="contact">
-		<h3>contact</h3>
-		<p>contact@BrandFlowr.nl</p>
-		<p>+31 6 41 22 00 44</p>
-		<div class="socials">
-			<h3>Follow us</h3>
-			<span>
-				<a href="https://www.facebook.com/profile.php?id=61557368016442" target="_blank"
-					><img class="socials-icon" src="assets/facebook.svg" alt="A link to our facebook page" loading="lazy" /></a
-				>
-			</span>
-			<span>
-				<a href="https://www.instagram.com/brandflowr/" target="_blank"
-					><img class="socials-icon" src="assets/instagram.svg" alt="A link to our instagram account" loading="lazy" /></a
-				>
-			</span>
-			<span>
-				<a href="https://www.linkedin.com/in/brandflowr-marketing-4530812b6/" target="_blank"
-					><img class="socials-icon" src="assets/linkedin.svg" alt="A link to our linkedin account" loading="lazy" /></a
-				>
-			</span>
+	<div class="footer-container">
+		<div class="footer-info">
+			<h2>Contact Us</h2>
+			<p>Email: <a href="mailto:contact@brandflowr.nl">contact@brandflowr.nl</a></p>
+			<p>Phone: <a href="tel:+31641220044">+31 6 41 22 00 44</a></p>
+			<p>KVK: 94536457</p>
+			<p>BTW: NL866813032B01</p>
 		</div>
+
+		<div class="footer-social">
+			<h2>Follow Us</h2>
+			<ul class="social-icons">
+				<li>
+					<a href="https://www.facebook.com/profile.php?id=61557368016442"
+						><img
+							src="https://cdn-icons-png.flaticon.com/512/733/733547.png"
+							alt="Click to go to our Facebook page"
+						/></a
+					>
+				</li>
+				<li>
+					<a href="https://www.instagram.com/brandflowr/"
+						><img
+							src="https://cdn-icons-png.flaticon.com/512/2111/2111463.png"
+							alt="Click to go to our Instagram page"
+						/></a
+					>
+				</li>
+				<li>
+					<a href="https://www.linkedin.com/company/brandflowr/"
+						><img
+							src="https://cdn-icons-png.flaticon.com/512/145/145807.png"
+							alt="Click to go to our LinkedIn page"
+						/></a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="footer-bottom">
+		<p>&copy; 2024 BrandFlowr. All rights reserved.</p>
 	</div>
 </section>
 
 <style>
 	section {
+		background-color: var(--services-background-color);
+		color: var(--text-color);
+		padding: 50px 20px;
 		display: flex;
-		justify-content: space-around;
+		flex-direction: column;
+		justify-content: space-between;
+	}
+
+	.footer-container {
+		display: flex;
+		justify-content: center; /* Center the items horizontally */
+		gap: 40px; /* Increase gap for more space between items */
+		flex-wrap: wrap;
+	}
+
+	.footer-info,
+	.footer-social {
+		flex: 1 1 300px;
+		text-align: center; /* Center text within each section */
+	}
+
+	.footer-info h2,
+	.footer-social h2 {
+		margin-bottom: 10px;
+		font-size: 1.5rem;
+		color: var(--services-text-color);
+	}
+
+	.footer-info p,
+	.footer-social ul {
+		margin: 0 0 10px;
+		font-size: 1rem;
+	}
+
+	.footer-info a,
+	.footer-social a {
+		color: var(--text-color);
+		text-decoration: none;
+	}
+
+	.footer-social ul {
+		list-style: none;
+		padding: 0;
+		display: flex;
+		justify-content: center; /* Center icons in social section */
+		gap: 15px;
+	}
+
+	.footer-social ul li {
+		display: inline-block;
+	}
+
+	.footer-social img {
+		width: 4em;
+		margin-top: 1em;
+		filter: grayscale(100%);
+		transition:
+			filter 0.1s ease-in-out,
+			transform 0.1s ease;
+	}
+
+	.footer-social img:hover {
+		filter: none;
+		transform: scale(1.1);
+	}
+
+	.footer-bottom {
+		text-align: center;
 		margin-top: 30px;
-		background-color: var(--text-color);
-		padding-top: 30px;
-		padding-bottom: 30px;
-		color: #38424f;
-		bottom: 0;
+		border-top: 1px solid var(--line-color);
+		padding-top: 15px;
 	}
 
-	.iconsvg {
-		margin-right: -16rem;
+	.footer-bottom p {
+		margin: 0;
+		font-size: 0.9rem;
 	}
 
-	.wavefooter {
-		display: block;
-		margin-bottom: -5rem;
-	}
-
-	.logo-title {
-		font-size: 3.6rem;
-		letter-spacing: 0.03rem;
-		font-family: 'Anton', sans-serif;
-		font-weight: 400;
-		font-style: normal;
-	}
-
-	.logo {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.socials {
-		margin-top: 2rem;
-	}
-
-	.socials h3 {
-		margin-bottom: 1rem;
-	}
-
-	span {
-		margin: 5px;
-	}
-
-	h3 {
-		text-transform: uppercase;
-		font-size: 1.35rem;
-	}
-
-	.socials-icon {
-		width: 4rem;
-		height: auto;
-		background-color: var(--background-color);
-		color: var(-background-color);
-		border-radius: 50%;
-	}
-
-	@media only screen and (max-width: 1250px) {
-		.socials-icon {
-			width: 3rem;
-		}
-
-		h3 {
-			font-size: 1.1rem;
-		}
-
-		.logo-title {
-			font-size: 2.5rem;
-		}
-	}
-
-	@media only screen and (max-width: 600px) {
-		section {
+	@media (max-width: 768px) {
+		.footer-container {
 			flex-direction: column;
 			align-items: center;
 		}
-		h3 {
-			text-align: center;
-		}
-
-		.wavefooter {
-			margin-bottom: -2rem;
-		}
-		.contact {
-			margin-top: 1rem;
-		}
-
-		span {
-			align-items: center;
-		}
 	}
 
-	@media only screen and (max-width: 390px) {
-		h3 {
-			font-size: 0.9rem;
+	@media (max-width: 480px) {
+		section {
+			padding: 30px 10px;
 		}
 
-		.iconsvg {
-			width: 90%;
-			margin-right: -13.5rem;
+		.footer-info h2,
+		.footer-social h2 {
+			font-size: 1.3rem;
+		}
+
+		.footer-info p,
+		.footer-social ul {
+			font-size: 0.9rem;
 		}
 	}
 </style>
